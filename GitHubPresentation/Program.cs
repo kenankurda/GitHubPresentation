3d29cc68aaa609feebca23b467566e939d5a8abd
kenan <kenan@LAPTOP-5KTTCAE1>
2020-03-27 15:30:28 +0100

    Added seconf line

--- GitHubPresentation/Program.cs
+++ GitHubPresentation/Program.cs
@@ -11,6 +11,7 @@ namespace GitHubPresentation
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world");
+            Console.WriteLine("Second Line");
         }
     }
 }
